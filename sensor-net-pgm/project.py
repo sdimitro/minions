eb0a709054b3a205a34ff1bb8855ed5df77f7cd3
Serapheim Dimitropoulos <serapheimd@gmail.com>
2016-03-15 20:03:10 -0400

    pgm sensor network - write_csv_data introduced

--- sensor-net-pgm/project.py
+++ sensor-net-pgm/project.py
@@ -1,5 +1,17 @@
-from numpy import genfromtxt
+import numpy as np
 
 def read_csv_data(filename):
-    return genfromtxt(filename, delimiter=',', skip_header=1)[:,1:]
+    return np.genfromtxt(filename, delimiter=',', skip_header=1)[:,1:]
+
+def write_csv_data(filename, data):
+    generated_header = "TODO"
+    # generated_sensor_ids = np.arange(0, 50)
+    # add generated row and column to data
+    np.savetxt(filename,
+               data,
+               #np.insert(data, 0, generated_sensor_ids, axis=1),
+               fmt='%.2E',
+               delimiter=',',
+               header=generated_header,
+               comments='')
 
