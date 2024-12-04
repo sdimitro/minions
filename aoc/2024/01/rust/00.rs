043e25228e31804a7f5613ed5999b972786128dd
Serapheim Dimitropoulos <serapheimd@gmail.com>
2024-12-04 09:22:36 -0800

    aoc:01:rust cleanup

--- aoc/2024/01/rust/00.rs
+++ aoc/2024/01/rust/00.rs
@@ -1,39 +1,41 @@
+#![feature(binary_heap_into_iter_sorted)]
+
 use std::collections::BinaryHeap;
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{Lines, BufReader, BufRead};
 use std::path::Path;
 
-// The output is wrapped in a Result to allow matching on errors.
+use anyhow::Result;
+
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(BufReader::new(file).lines())
 }
 
-fn compute_absdiff_sum(file_path: &str) -> io::Result<u32> {
-    let mut sorted_column_0 = BinaryHeap::new();
-    let mut sorted_column_1 = BinaryHeap::new();
+fn compute_absdiff_sum(file_path: &str) -> Result<u32> {
+    let mut sorted_left_column = BinaryHeap::new();
+    let mut sorted_right_column = BinaryHeap::new();
     for line in read_lines(file_path)?.flatten() {
         let mut tokens = line.split_whitespace();
-        sorted_column_0.push(tokens.next().unwrap().parse::<u32>().unwrap());
-        sorted_column_1.push(tokens.next().unwrap().parse::<u32>().unwrap());
+        sorted_left_column.push(tokens.next().unwrap().parse::<u32>()?);
+        sorted_right_column.push(tokens.next().unwrap().parse()?);
     }
-
-    let mut sum = 0u32;
-    while !sorted_column_0.is_empty() {
-        sum += sorted_column_0.pop().unwrap().abs_diff(sorted_column_1.pop().unwrap());
-    }
-    return Ok(sum);
+    Ok(sorted_left_column
+        .into_iter_sorted()
+        .zip(sorted_right_column.into_iter_sorted())
+        .fold(0, |acc, (l, r)| acc.checked_add(l.abs_diff(r)).unwrap()))
 }
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    for arg in args {
+    for arg in env::args().skip(1) {
         match compute_absdiff_sum(&arg) {
             Ok(sum) => println!("{arg} - {sum}"),
-            Err(s) => println!("error: {arg}: {s}"),
+            Err(s) => eprintln!("error: {arg}: {s}"),
         }
     }
 }
--- aoc/2024/01/rust/01.rs
+++ aoc/2024/01/rust/01.rs
@@ -1,42 +1,40 @@
+use anyhow::Result;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::BufRead;
+use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
 
-fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<std::io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
-    Ok(std::io::BufReader::new(file).lines())
+    Ok(BufReader::new(file).lines())
 }
 
-fn compute_similarity_score(file_path: &str) -> std::io::Result<u32> {
+fn compute_similarity_score(file_path: &str) -> Result<u32> {
     let mut freq = HashMap::new();
     let mut score = HashMap::new();
-
     for line in read_lines(file_path)?.flatten() {
         let mut tokens = line.split_whitespace();
 
-        let key = tokens.next().unwrap().parse::<u32>().unwrap();
-        *freq.entry(key).or_insert(0) += 1;
-        let value = tokens.next().unwrap().parse::<u32>().unwrap();
+        let key = tokens.next().unwrap().parse::<u32>()?;
+        *freq.entry(key).or_insert(0) += 1u32;
+        let value = tokens.next().unwrap().parse::<u32>()?;
         *score.entry(value).or_insert(0) += value;
     }
-    
-    let mut similarity = 0u32;
-    for (k, count) in freq.drain() {
-            similarity += count * score.get(&k).unwrap_or(&0);
-    }
-    return Ok(similarity);
+    Ok(freq.into_iter().fold(0, |acc, (k, count)| {
+        acc.checked_add(count.checked_mul(*score.get(&k).unwrap_or(&0)).unwrap())
+            .unwrap()
+    }))
 }
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    for arg in args {
+    for arg in env::args().skip(1) {
         match compute_similarity_score(&arg) {
             Ok(similarity_score) => println!("{arg} - {similarity_score}"),
-            Err(s) => println!("error: {arg}: {s}"),
+            Err(s) => eprintln!("error: {arg}: {s}"),
         }
     }
 }
